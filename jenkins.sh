dfc2183a6608bf8a7ae26416f419a3ae8f0a8009
yamunaregalla <yamunaregalla0450@gmail.com>
2026-05-25 12:06:21 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@ sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
+sudo yum install java-21-amazon-corretto -y
 sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
